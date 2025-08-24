90a0225af9c5a178cfce96f03649969ba8d3ea66
Naxdia <naxdiax@gmail.com>
2025-08-24 18:10:39 +1000

    Intial commit message

---
+++ Primitive Data Types in Python.py
@@ -0,0 +1,37 @@
+#Primitive Data Types in Python
+
+#String: This is a sequence of characters
+name = "John Doe"
+print("Name: ", name)
+
+#Integer: This is a whole number 
+age = 25
+print("Age: ", age)
+
+#Float: This is a number with a decimal point
+height = 6.1
+print("Height: ", height)   
+
+#Boolean: This represents True or False values
+is_learning = True
+print("Is learning Python: ", is_learning)
+
+#List: This is an ordered collection of items
+fruits = ["apple", "banana", "cherry"]
+print("Fruits: ", fruits)
+
+#Tuple: This is an immutable (nothing can be changed) ordered collection of items
+colors = ("red", "green", "blue")
+print("Colors: ", colors)
+
+#Dictionary: This is a collection of key-value pairs
+person = {
+    "name": "Alice", "age": 30, "location": "Wonderland"
+}
+print("Person (Name/Age/Location): ", person["location"])
+
+#Set: This is an unordered collection of unique items
+random_sequence = {1, "Fish", 3.14, "Toyota", 32}
+print("Unique Numbers: ", random_sequence)
+
+

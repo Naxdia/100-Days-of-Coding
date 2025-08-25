f40c209c063c6001467021c5f49b3ff9483cffc4
Naxdia <naxdiax@gmail.com>
2025-08-25 21:52:38 +1000

    Day 3 - Control Flow Statements in Python

--- Primitive Data Types in Python.py
+++ Primitive Data Types in Python.py
@@ -1,18 +1,31 @@
-#Tip calculator
+#Day 3: Control Flow
 
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = float(input("What percentage tip would you like to give? "))
-split = int(input("How many people to split the bill? "))
+print("Welcome to Treasure Island.")
+print("Your objective is to naviagate through the island to find the treasure.")
 
-total_bill = bill + (bill * tip / 100)
+#First decision
+choice1 = input("""You're at a crossroad. Where do you want to go? 
+            Type "left" or "right"\n""").lower()
 
-round(total_bill, 2)
 
-if split == 0:
-    print("Number of people cannot be zero.")
-elif split == 1:
-    print(f"You should pay: ${total_bill}")
+#The user's first decision is held in the variable: choice1.
+#If they chose "left", they are prompted with a second decision.
+#If they chose "right" or anything else, they lose the game.
+if choice1 == "left":
+    choice2 = input("""You've come to a lake. There is an island in the middle of the lake.
+            Type "wait" to wait for a boat. Type "swim" to swim across.\n""").lower()
+    if choice2 == "wait":
+        choice3 = input("""You arrive at the island unharmed. There is a house with 3 doors.
+                A red door, a blue door and a yellow door. Which colour do you choose?\n""").lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
 else:
-    bill_per_person = total_bill / split
-    print(f"Each person should pay: ${bill_per_person}")
+    print("You fell into a hole. Game Over.")

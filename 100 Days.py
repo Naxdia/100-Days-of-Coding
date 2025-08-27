fafbee8bd3c0d28b8ed40a64246531b5bd492ed5
Naxdia <naxdiax@gmail.com>
2025-08-27 22:22:41 +1000

    Day 5 - Password Generator

--- 100 Days.py
+++ 100 Days.py
@@ -1,39 +1,36 @@
-#Day 4: Randomisation and Python Lists
-#Rock, Paper, Scissors will have 4 outcomes: Paper vs Scissors (Scissors wins), Paper vs Rock (Paper wins), Rock vs Scissors (Rock wins) and a draw if both players choose the same item. 
-#However, in terms of the player, there are only 3 results: Win, Lose or Draw.
-
+#Day 5: Password Generator Project
 import random
-#First, we need to get the user's choice
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if user_choice == 0:
-    choice_name = "Rock"
-    print("You chose Rock")
-elif user_choice == 1:
-    choice_name = "Paper"
-    print("You chose Paper")
-elif user_choice == 2:
-    choice_name = "Scissors"
-    print("You chose Scissors")
-else:
-    print("You did not type a valid number, please try again.")
 
-#The computer's choice will be randomly generated using the random module. Because the computer is limited to 3 choices, we can use else instead of elif for the last option.
-computer_choice = random.randint(0, 2)
-if computer_choice == 0:
-    computer_choice_name = "Rock"
-    print("Computer chose Rock")
-elif computer_choice == 1:
-    computer_choice_name = "Paper"
-    print("Computer chose Paper")
-else:
-    computer_choice_name = "Scissors"
-    print("Computer chose Scissors")
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+print("Welcome to the random password generator!")
 
-if choice_name == computer_choice_name:
-    print("It's a draw")    
-#This line of code reprensents all the winning conditions for the user.
-elif (choice_name == "Rock" and computer_choice_name == "Scissors") or (choice_name == "Paper" and computer_choice_name == "Rock") or (choice_name == "Scissors" and computer_choice_name == "Paper"):
-    print("You win!")
-else:
-    print("You lose")
+#Ask user for number of letters, numbers and symbols
+num_letters = int(input("How many letters would you like in your password?\n"))
+num_numbers = int(input("How many numbers would you like?\n"))
+num_symbols = int(input("How many symbols would you like?\n"))
+
+#Empty list to hold password characters
+password_list = []
+
+#For loop cycles through each character type and appends a random choice to the password list
+for char in range(0, num_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, num_numbers):
+    password_list.append(random.choice(numbers))
+
+for char in range(0, num_symbols):
+    password_list.append(random.choice(symbols))
+
+#Empty string to hold final password
+password = ""
+
+#Shuffle the password list to ensure randomness and then concatenate each character to form the final password
+random.shuffle(password_list)
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")

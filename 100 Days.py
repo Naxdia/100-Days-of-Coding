8367505d9b14ea8ce905c6308eb1d7e99c5f71c1
Naxdia <naxdiax@gmail.com>
2025-12-07 21:20:52 +1000

    Coming back to learning how to code in Python

--- 100 Days.py
+++ 100 Days.py
@@ -1,41 +1,53 @@
-#Day 9 - Secret Auction
+#Day 10 - Calculator Project
 
-print("Welcome to the secret auction.")
-#Dictionary to store bidders and bids
+#Defining functions for each operation
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
 
-#Creating a fuction to determine the highest bidder
-def highest_bidder(bidding_dict):
-    #Set initial highest amount to 0
-    highest_amount = 0
-    #For loop to go through each bidder in the bidding dictionary
-    for bidder in bidding_dict:
-        #Bid amount is retrieved from dictionary's key
-        bid_amount = bidding_dict[bidder]
-        if bid_amount > highest_amount:
-            #If current bid amount is higher than highest amount, update highest amount and winner
-            highest_amount = bid_amount
-            winner = bidder
-    print(f"The winner is {winner} with a bid of ${highest_amount}")
+operations = {"+": add,
+              "-": subtract,
+              "*": multiply,
+              "/": divide
+              }
+### operations["*"] is equivalent to the word 'multiply' which calls that function
+#print(operations["*"](4,8))
 
-    for i in bidding_dict:
-        print(f"{name} bid ${bidding_dict[name]}")
 
-bids = {}
-#more_bidders act a flag to continue or stop the bidding process
-more_bidders = True
-highest_bid = 0
-while more_bidders:
-    name = input("What is your name?: ")
-    amount = int(input("What is your bid?: $"))
 
-    #bids dictionary updated with name and amount
-    bids[name] = amount
-    continue_bidding = input("Are there any other bidders? Type 'Yes' or 'No'.\n").lower()
-    if continue_bidding == "yes":
-        more_bidders = True
-    else:
-        more_bidders = False
-        highest_bidder(bids)
+
+def calculator():
+    num1 = float(input("What is the first number?: "))
+    should_continue = True
+    #For loop to iterate through the dictionary and print the keys for the user to see what operations are available
+    while should_continue:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = float(input("What is the second number?: "))
+        result = (operations[operation_symbol](num1, num2))
+        #Displaying the entire equation to the user
+        print(f"{num1} {operation_symbol} {num2} = {result}")
+
+        choice = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation or 'exit' to leave the program: ")
+        if choice == "y":
+            num1 = result
+        elif choice == "n":
+            print("\n"*20)
+            #Recursive call to restart the calculator function
+            calculator()
+        else:
+            choice == "exit"
+            return
+              
+        
+calculator()
+
 
 
 
